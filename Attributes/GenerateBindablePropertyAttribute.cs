e0bf6fabb3d44ae5c120df5f4cabcbc06edfef92
Christian Strydom <christian.strydom@gmail.com>
2025-04-24 16:27:09 +0200

    Add source generator and attribute projects to generate bindable properties boilerplate

---
+++ Attributes/GenerateBindablePropertyAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BindablePropertyGenerator.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    public class GenerateBindablePropertyAttribute : Attribute
+    {
+        public Type Type;
+
+        public GenerateBindablePropertyAttribute(Type type)
+        {
+            Type = type;
+        }
+    }
+}
---
+++ SourceGenerators/BindablePropertySourceGenerator.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SourceGenerators
+{
+    [Generator]
+    public class BindablePropertySourceGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // Register a syntax receiver that will be created for each generation pass
+            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if (!(context.SyntaxContextReceiver is SyntaxReceiver receiver))
+                return;
+            
+            var attributeSymbol = context.Compilation.GetTypeByMetadataName("BindablePropertyGenerator.Attributes.GenerateBindablePropertyAttribute");
+
+            foreach (IGrouping<INamedTypeSymbol, IFieldSymbol> group in receiver.Fields.GroupBy<IFieldSymbol, INamedTypeSymbol>(f => f.ContainingType, SymbolEqualityComparer.Default))
+            {
+                string classSource = ProcessClass(group.Key, group.ToList(), attributeSymbol, context);
+                context.AddSource($"{group.Key.Name}.g.cs", SourceText.From(classSource, Encoding.UTF8));
+            }
+        }
+
+        private string ProcessClass(INamedTypeSymbol classSymbol, List<IFieldSymbol> fields, ISymbol attributeSymbol, GeneratorExecutionContext context)
+        {
+            if (!classSymbol.ContainingSymbol.Equals(classSymbol.ContainingNamespace, SymbolEqualityComparer.Default))
+            {
+                return null; //TODO: issue a diagnostic that it must be top level
+            }
+
+            string namespaceName = classSymbol.ContainingNamespace.ToDisplayString();
+
+            // begin building the generated source
+            StringBuilder source = new StringBuilder($@"
+namespace {namespaceName}
+{{
+    public partial class {classSymbol.Name}
+    {{");
+
+            // create properties for each field 
+            foreach (IFieldSymbol fieldSymbol in fields)
+            {
+                ProcessField(source, fieldSymbol, attributeSymbol);
+            }
+
+            source.Append($@"
+
+        public static BindableProperty CreateBindableProperty<T>(
+            string propertyName, T defaultValue, BindableProperty.BindingPropertyChangedDelegate propertyChanged = null, BindingMode defaultBindingMode = BindingMode.TwoWay)
+        {{
+            return BindableProperty.Create(
+                propertyName: propertyName,
+                returnType: typeof(T),
+                declaringType: typeof({classSymbol.Name}),
+                defaultValue: defaultValue,
+                defaultBindingMode: defaultBindingMode,
+                propertyChanged: propertyChanged
+            );
+        }}
+    }} 
+}}"
+            );
+            return source.ToString();
+        }
+
+        private void ProcessField(StringBuilder source, IFieldSymbol fieldSymbol, ISymbol attributeSymbol)
+        {
+            string fieldName = fieldSymbol.Name;
+            var value = fieldSymbol.ConstantValue;
+            ITypeSymbol fieldType = fieldSymbol.Type;
+
+            AttributeData attributeData = fieldSymbol.GetAttributes().Single(ad => ad.AttributeClass.Equals(attributeSymbol, SymbolEqualityComparer.Default));
+
+            if (attributeData.ConstructorArguments.IsEmpty || attributeData.ConstructorArguments[0].Value is not INamedTypeSymbol typeSymbol)
+            {
+                return;
+            }
+
+            var overridenNameOpt = attributeData.NamedArguments.SingleOrDefault(kvp => kvp.Key == "PropertyName").Value;
+
+            var propertyName = ChooseName(fieldName, overridenNameOpt);
+            if (propertyName.Length == 0 || propertyName == fieldName)
+            {
+                return;
+            }
+
+            source.Append($@"
+        public {typeSymbol} {propertyName} 
+        {{
+            get => ({typeSymbol})GetValue({propertyName}Property);
+            set => SetValue({propertyName}Property, value);
+        }}
+        ");
+
+            string ChooseName(string fieldName, TypedConstant overridenNameOpt)
+            {
+                if (!overridenNameOpt.IsNull)
+                {
+                    return overridenNameOpt.Value.ToString();
+                }
+
+                fieldName = fieldName.Replace("Property", "");
+                if (fieldName.Length == 0)
+                    return string.Empty;
+
+                return fieldName;
+            }
+
+        }
+
+        class SyntaxReceiver : ISyntaxContextReceiver
+        {
+            public List<IFieldSymbol> Fields { get; } = new List<IFieldSymbol>();
+
+            /// <summary>
+            /// Called for every syntax node in the compilation, we can inspect the nodes and save any information useful for generation
+            /// </summary>
+            public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+            {
+                if (context.Node is FieldDeclarationSyntax fieldDeclarationSyntax
+                    && fieldDeclarationSyntax.AttributeLists.Count > 0)
+                {
+                    foreach (VariableDeclaratorSyntax variable in fieldDeclarationSyntax.Declaration.Variables)
+                    {
+                        IFieldSymbol fieldSymbol = context.SemanticModel.GetDeclaredSymbol(variable) as IFieldSymbol;
+                        if (fieldSymbol.GetAttributes().Any(ad => ad.AttributeClass.ToDisplayString() == "BindablePropertyGenerator.Attributes.GenerateBindablePropertyAttribute"))
+                        {
+                            Fields.Add(fieldSymbol);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
